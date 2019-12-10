dae9028dcd60cd5b3729a80687091e1121c2ceee
Devon James Wright <devonjamess.wright@gmail.com>
2019-12-10 17:02:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,21 +22,21 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  reasons_I_am_against_conforming = ["My", "arrays", "aren't", "Wrong"]
+  reasons_I_am_against_conforming = ["wow", "arrays", "aren't", "Wrong"]
   reasons_I_am_against_conforming.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  reasons_I_am_against_conforming = ["My", "arrays", "aren't", "Wrong"]
-  reasons_I_am_against_conforming.index ("arrays")
+  reasons_I_am_against_conforming = ["am", "arrays", "aren't", "Wrong"]
+  reasons_I_am_against_conforming.index (0)
 end
 
 def retrieve_first_element_from_array(array)
-reasons_I_am_against_conforming = ["My", "arrays", "aren't", "Wrong"]
+reasons_I_am_against_conforming = ["wow", "arrays", "aren't", "Wrong"]
 reasons_I_am_against_conforming.first
 end
 
 def retrieve_last_element_from_array(array)
-  reasons_I_am_against_conforming = ["My", "arrays", "aren't", "Wrong"]
+  reasons_I_am_against_conforming = ["My", "arrays", "aren't", "arrays"]
   reasons_I_am_against_conforming.last
 end
